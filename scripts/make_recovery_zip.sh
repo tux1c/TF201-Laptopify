040412a38c0bb68d891872c9c5c15a2a5ef6b813
Yan A. <Tux1c@users.noreply.github.com>
2014-11-16 20:17:19 +0200

    Delete make_recovery_zip.sh
    Unneeded

--- scripts/make_recovery_zip.sh
+++
@@ -1,8 +0,0 @@
-script_dir=$(dirname $0)
-cd "$script_dir"
-
-cd ../kernel_chooser/edify
-cp ../../test/test.blob ./kernel.blob
-zip -r ../../test/test.zip *
-rm kernel.blob
-#adb push ../test/recovery.zip /sdcard/
